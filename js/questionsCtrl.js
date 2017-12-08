d8d921c8a6bb86b1430c8943badb899e9da833a9
Devin Jackson <Devidu@Devins-MacBook-Pro.local>
2017-12-08 13:49:11 -0600

    added logic for modal

--- js/questionsCtrl.js
+++ js/questionsCtrl.js
@@ -1,12 +1,38 @@
 angular.module("app").controller("questionsCtrl",function($scope, $http){
     $scope.test="Testing 123";
-<<<<<<< HEAD
-    $scope.newQuestion = {};
-=======
+    $scope.editQuestion ='';
+    $scope.option1 = ""
+    $scope.option2 = ""
+    $scope.option3 = ""
+    $scope.option4 = ""
+    $scope.newQuestion = {
+        animal: "",
+        difficulty: 2,
+        correct_answer: 1,
+        options: {
+            1: $scope.option1,
+            2: $scope.option2,
+            3: $scope.option3,
+            4: $scope.option4
+        },
+        question: "",
+        saveNew(){
+            console.log("saveNew ran")
+        },
+        cancelSave(){
+            $scope.modalAddOrEdit = false;
+            console.log("cancelSave ran")
+        },
+        showModal(){
+            $scope.modalAddOrEdit = true;
+            console.log("showModal ran", modalAddOrEdit)
+        }
+    };
+    
+
     $scope.quiz = $http
       .get("https://practiceapi.devmountain.com/api/trivia/questions")
       .then(response => {
           $scope.questions = response.data;
           console.log(response.data)})
->>>>>>> master
 });
