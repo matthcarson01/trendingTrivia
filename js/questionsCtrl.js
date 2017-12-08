7e969c6e1d367bb3f5e1e57e535b3aab3c68d801
Leah Dombrovsky <leahliya59@gmail.com>
2017-12-08 16:21:33 -0600

    color chane works

--- js/questionsCtrl.js
+++ js/questionsCtrl.js
@@ -8,17 +8,20 @@ angular.module("app").controller("questionsCtrl", function($scope, $http) {
     });
 
   $scope.newColor = { "background-color": "white" };
-  $scope.correctAnswer = function(key, correct_answer) {
+  $scope.correctAnswer = function(key, correct_answer, id) {
     // if(question.option[key] ===question.correct_answer){
+    $scope.answerId = id;
     console.log("Key", key);
+    console.log("id", id);
     console.log("Answer", correct_answer);
     if (key == correct_answer) {
       $scope.newColor = { "background-color": "green" };
-      $scope.isCorrect = true;
+      //   $scope.isCorrect = true;
+      $scope.chosen_answer = correct_answer;
       //   console.log("Correct answer");
     } else {
       $scope.newColor = { "background-color": "red" };
-      $scope.isCorrect = false;
+      //   $scope.isCorrect = false;
     }
   };
 });
